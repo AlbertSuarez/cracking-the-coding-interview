34d97333749c3545bb3af267e1591b57eb4a7d03
Albert Suarez <alsumo95@gmail.com>
2020-02-08 16:52:40 +0100

    Implemented Question abstract class for all questions

---
+++ src/helper/question.py
@@ -0,0 +1,24 @@
+import abc
+import time
+
+
+class Question:
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, name: str):
+        self.name = name
+        self.result = None
+
+    def __enter__(self):
+        self.start_time = time.time()
+
+    def __exit__(self, *args):
+        self.end_time = time.time()
+        self.total_time = self.end_time - self.start_time
+        print(f'[{self.name}] - Result: {self.result}')
+        print(f'[{self.name}] - Total time: {self.total_time:.6f}s')
+
+    @abc.abstractmethod
+    def solve(self):
+        pass
